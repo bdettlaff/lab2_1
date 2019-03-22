04337992dcb0f9bbf684064d177f668cd9438e07
dettlaffb <209295@edu.p.lodz.pl>
2019-03-22 13:27:28 +0100

    testy_V1 Added Test - Sequence Length = 1 and Element is not in Sequence

--- src/test/java/edu/iis/mto/bsearch/BinaryResultTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinaryResultTest.java
@@ -6,11 +6,20 @@ import static org.junit.Assert.*;
 
 public class BinaryResultTest {
 
+
     @Test
     public void shouldReturnTrueIfOneElementIsInOneElementSequence() {
         int[] sequence = {3};
         int key = 3;
         SearchResult searchResult = BinarySearch.search(key, sequence);
         assertEquals(1, searchResult.getPosition());
+    }
+
+    @Test
+    public void shouldReturnTrueIfOneElementDoesNoTExistInOneElementSequence() {
+        int[] sequence = {10};
+        int key = 14;
+        SearchResult searchResult = BinarySearch.search(key, sequence);
+        assertEquals(-1, searchResult.getPosition());
     }
 }
